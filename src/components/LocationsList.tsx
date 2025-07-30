1166af8809738092a7c9d1e202121fe8057408ea
Martijn Atema <martijn@atema.one>
2025-07-30 23:37:11 +0200

    Implement gpx import

--- src/components/LocationsList.tsx
+++ src/components/LocationsList.tsx
@@ -1,9 +1,11 @@
 import { downloadGpx } from "../state/export-gpx";
+import { uploadGpx } from "../state/import-gpx";
 import { locations } from "../state/locations";
 
 export const LocationsList = () => (
   <div>
     <button onClick={() => downloadGpx()}>Download</button>
+    <button onClick={() => uploadGpx()}>Upload</button>
     {locations.value.map((loc) => (
       <div key={loc.id}>
         {loc.lat}, {loc.lon}
---
+++ src/state/import-gpx.ts
@@ -0,0 +1,52 @@
+import { XMLParser } from "fast-xml-parser";
+import { addLocation } from "./locations";
+
+const alwaysArray = ["gpx.trk", "gpx.trk.trkseg", "gpx.trk.trkseg.trkpt"];
+
+const xmlParser = new XMLParser({
+  ignoreAttributes: false,
+  attributeNamePrefix: "$",
+  isArray: (_, jpath) => alwaysArray.includes(jpath),
+});
+
+export const parseGpx = (contents: string, filename?: string) => {
+  try {
+    const { gpx } = xmlParser.parse(contents);
+
+    if (!gpx) {
+      throw new Error("No gpx data found in file");
+    }
+
+    // const name: string = `${gpx?.metadata?.name}` || filename || "";
+
+    const points = gpx.trk
+      .flatMap((trk: any) => trk.trkseg || [])
+      .flatMap((seg: any) => seg.trkpt || []);
+
+    if (points.length == 0) {
+      throw new Error("No track data found in file");
+    }
+
+    points.forEach((el: any) => {
+      addLocation(el["$lon"], el["$lat"]);
+    });
+
+    console.log(points);
+  } catch (e) {
+    window.alert(`${e}`);
+  }
+};
+
+export const uploadGpx = () => {
+  const input = document.createElement("input");
+  input.type = "file";
+  input.accept = "application/gpx+xml";
+
+  input.addEventListener("change", async () => {
+    if (input.files?.length) {
+      parseGpx(await input.files[0].text(), input.files[0].name);
+    }
+  });
+
+  input.click();
+};

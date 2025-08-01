72fa8b9bcc8411c67ff21542c71e17aa9568dffd
Martijn Atema <martijn@atema.one>
2025-08-01 19:51:50 +0200

    Use virtuoso for a faster locations list

--- src/components/list/LocationsList.tsx
+++ src/components/list/LocationsList.tsx
@@ -2,14 +2,20 @@ import { downloadGpx } from "../../state/export-gpx";
 import { uploadGpx } from "../../state/import-gpx";
 import { locations } from "../../state/locations";
 import { LocationItem } from "./LocationItem";
-import "./LocationsList.module.css";
+import { Virtuoso } from "react-virtuoso";
+import style from "./LocationsList.module.css";
 
 export const LocationsList = () => (
-  <div>
-    <button onClick={downloadGpx}>Download</button>
-    <button onClick={uploadGpx}>Upload</button>
-    {locations.value.map((location) => (
-      <LocationItem location={location} />
-    ))}
+  <div class={style.sidepanel}>
+    <div>
+      <button onClick={downloadGpx}>Download</button>
+      <button onClick={uploadGpx}>Upload</button>
+    </div>
+    <div class={style.list}>
+      <Virtuoso
+        data={locations.value}
+        itemContent={(_, loc) => <LocationItem key={loc.id} location={loc} />}
+      />
+    </div>
   </div>
 );
